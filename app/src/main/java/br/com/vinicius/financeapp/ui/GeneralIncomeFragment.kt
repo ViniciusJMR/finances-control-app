bc8e98f92cb193793d25c9bdeecc4b76a361551a
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-08-12 17:41:08 -0300

    Removed styles from text view & Implemented viewpager2 on ActivityMain

--- app/src/main/java/br/com/vinicius/financeapp/ui/GeneralIncomeFragment.kt
+++ app/src/main/java/br/com/vinicius/financeapp/ui/GeneralIncomeFragment.kt
@@ -1,21 +1,21 @@
 package br.com.vinicius.financeapp.ui
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.com.vinicius.financeapp.databinding.FragmentHomeScreenBinding
+import androidx.fragment.app.Fragment
+import br.com.vinicius.financeapp.databinding.FragmentGeneralIncomeBinding
 
-class HomeScreenFragment : Fragment() {
-
-    private lateinit var binding: FragmentHomeScreenBinding
+class GeneralIncomeFragment : Fragment() {
+    private lateinit var binding: FragmentGeneralIncomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHomeScreenBinding.inflate(inflater, container, false)
+        binding = FragmentGeneralIncomeBinding.inflate(inflater, container, false)
 
         return binding.root
     }
--- app/src/main/java/br/com/vinicius/financeapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/vinicius/financeapp/ui/MainActivity.kt
@@ -1,12 +1,32 @@
 package br.com.vinicius.financeapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import br.com.vinicius.financeapp.R
+import br.com.vinicius.financeapp.databinding.ActivityMainBinding
+import br.com.vinicius.financeapp.ui.adapter.TabsPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val adapter by lazy { TabsPagerAdapter(this) }
+
+override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.vpMain.adapter = adapter
+
+        /*
+            FIXME Find a better way to set icon on tabs
+         */
+        TabLayoutMediator(binding.tlTopMenu, binding.vpMain){ tab, position ->
+            when(position) {
+                0 -> tab.setIcon(R.drawable.ic_home_24)
+                else -> tab.setIcon(R.drawable.ic_money_24)
+            }
+        }.attach()
     }
 }
