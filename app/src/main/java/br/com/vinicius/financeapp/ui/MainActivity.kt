fe50e0e09498583e786de14a5c1c8d071dde913b
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-08-16 19:59:23 -0300

    Changed tab menu to bottom menu

--- app/src/main/java/br/com/vinicius/financeapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/vinicius/financeapp/ui/MainActivity.kt
@@ -2,9 +2,14 @@ package br.com.vinicius.financeapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import br.com.vinicius.financeapp.R
 import br.com.vinicius.financeapp.databinding.ActivityMainBinding
 import br.com.vinicius.financeapp.ui.adapter.TabsPagerAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayoutMediator
 
 
@@ -13,20 +18,14 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val adapter by lazy { TabsPagerAdapter(this) }
 
-override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.vpMain.adapter = adapter
-
-        /*
-            FIXME Find a better way to set icon on tabs
-         */
-        TabLayoutMediator(binding.tlTopMenu, binding.vpMain){ tab, position ->
-            when(position) {
-                0 -> tab.setIcon(R.drawable.ic_home_24)
-                else -> tab.setIcon(R.drawable.ic_money_24)
-            }
-        }.attach()
+        val navCtrl = findNavController(R.id.fragmentContainerView)
+        binding.bnvMenu.setupWithNavController(navCtrl)
     }
+
+
+
 }
